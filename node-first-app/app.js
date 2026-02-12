a4b55420c82013a669d64ecdda50371541e6845d
Nimesh <lov.nimesh2024@lpu.in>
2026-02-13 03:42:48 +0530

    commit changes

--- node-first-app/app.js
+++ node-first-app/app.js
@@ -1,55 +1,7 @@
 import { createServer } from "http";
-import * as fs from "fs";
 
-const server = createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
+import routes from "./routes.js";
 
-  //route based server responsding
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message </title></head>");
-    res.write(`
-      <body>
-        <form action="/message" method="POST">
-          <input type = "text" name="message">
-          <button type="submit">Send</Button>
-        </form>
-      </body>`);
-    res.write("</html>");
-
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1].split("+").join(" ");
-      fs.writeFileSync("message.txt", message);
-      console.log(parsedBody);
-    });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-
-  //sending response
-  res.setHeader("content-type", "text/html");
-
-  res.write("<html>");
-  res.write("<head><title>My First Page </title></head>");
-  res.write("<body><h1>Hello from my node.js Server!</h1></body>");
-  res.write("</html>");
-
-  res.end();
-});
+const server = createServer(routes);
 
 server.listen(3000, "localhost");
