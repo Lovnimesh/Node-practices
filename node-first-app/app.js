733c466704fffcefe2218b4e3e1198f5cf0b3e53
Nimesh <lov.nimesh2024@lpu.in>
2026-02-19 04:52:39 +0530

    changing node.js content to express.js

--- node-first-app/app.js
+++ node-first-app/app.js
@@ -1,7 +1,16 @@
-import { createServer } from "http";
+"use strict";
+import express from "express";
 
-import routes from "./routes.js";
+const app = express();
 
-const server = createServer(routes);
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //Allow the request to continue to the next middleware in line
+});
 
-server.listen(3000, "localhost");
+app.use((req, res, next) => {
+  console.log("In the another middleware");
+  res.send("<h1>Hello from Express.js</h1>");
+});
+
+app.listen(3000, "localhost");
