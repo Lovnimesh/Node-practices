f5463568a88a355b167dce1540ce396bc5414206
Nimesh <lov.nimesh2024@lpu.in>
2026-02-24 04:30:26 +0530

    added the server side rendering content

--- node-first-app/app.js
+++ node-first-app/app.js
@@ -1,16 +1,21 @@
 "use strict";
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); //Allow the request to continue to the next middleware in line
-});
+import adminRoutes from "./routes/admin.js";
+import frontRoutes from "./routes/shop.js";
 
-app.use((req, res, next) => {
-  console.log("In the another middleware");
-  res.send("<h1>Hello from Express.js</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// here order of routes matters if we are using the app.use() methods in routes but doesn't matters if we are using app.get() or app.post();
+
+app.use("/admin", adminRoutes);
+app.use(frontRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000, "localhost");
