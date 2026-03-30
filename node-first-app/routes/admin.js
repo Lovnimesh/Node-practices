65408e0a49b2a6e24dc996f13fc1a190ea85c2d3
Nimesh <lov.nimesh2024@lpu.in>
2026-03-31 03:50:22 +0530

    learning node.js

--- node-first-app/routes/admin.js
+++ node-first-app/routes/admin.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 
 const router = express.Router();
+export const products = [];
 
 router.get("/add-product", (req, res, _next) => {
   res.sendFile(path.resolve("views", "add-product.html"));
@@ -9,6 +10,7 @@ router.get("/add-product", (req, res, _next) => {
 
 router.post("/add-product", (req, res) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
--- node-first-app/routes/shop.js
+++ node-first-app/routes/shop.js
@@ -1,9 +1,12 @@
 import express from "express";
 import path from "path";
 
+import { products } from "./admin.js";
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(products);
   res.sendFile(path.resolve("views", "shop.html"));
 });
 
